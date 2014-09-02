be7a4660c0dccc201d850b2be73a2759ea77d88c
Rui Abreu Ferreira <raf-ep@gmx.com>
2014-09-02 10:33:32 +0100

    Add new unittest
    - A broad unit test that tries to call as many methods as it can, mostly
      to detect potential pack/unpack errors

---
+++ test/tst_callallmethods.cpp
@@ -0,0 +1,81 @@
+#include <QLocalSocket>
+#include <QMetaMethod>
+#include <QtTest/QtTest>
+#include <neovimconnector.h>
+#include <auto/neovim.h>
+
+/**
+ * This is the closest I can get to writing a 5 minute unit test without
+ * putting any effort into this - basically we call all API methods that
+ * have no arguments.
+ *
+ * -- Don't take this too seriously, I just wanted to trigger some unpacking
+ *    bugs :D
+ */
+
+class TestCallAllMethods: public QObject
+{
+	Q_OBJECT
+
+protected slots:
+	void callAll();
+
+private slots:
+	void initTestCase();
+	void error(const QString& errmsg);
+
+private:
+	NeovimQt::NeovimConnector *m_c;
+	bool m_errors;
+};
+
+void TestCallAllMethods::initTestCase()
+{
+	m_errors = false;
+	QLocalSocket *s = new QLocalSocket();
+	s->connectToServer(QLatin1String("/tmp/neovim"));
+	Q_ASSERT(s->waitForConnected());
+	m_c = new NeovimQt::NeovimConnector(s);
+	Q_ASSERT(m_c->neovimObject());
+
+	connect(m_c, &NeovimQt::NeovimConnector::ready,
+			this, &TestCallAllMethods::callAll);
+	QTest::qWait(500);
+	Q_ASSERT(!m_errors);
+}
+
+void TestCallAllMethods::error(const QString& errmsg)
+{
+	qWarning() << errmsg;
+	m_errors = true;
+}
+
+void TestCallAllMethods::callAll()
+{
+	NeovimQt::Neovim *obj = m_c->neovimObject();
+	connect(m_c->neovimObject(), &NeovimQt::Neovim::error,
+			this, &TestCallAllMethods::error);
+	const QMetaObject *meta = obj->metaObject();
+	for (int i=0; i<meta->methodCount(); i++) {
+		QMetaMethod meth = meta->method(i);
+		if ( meth.methodType() != QMetaMethod::Slot ||
+				meth.access() != QMetaMethod::Public) {
+			continue;
+		}
+
+		if ( meth.parameterNames().size() == 0 && meth.name() != "deleteLater") {
+			qDebug() << "Calling" << meth.name();
+			meth.invoke(obj);
+		} else if ( meth.parameterNames().size() == 1 ) {
+			if ( meth.parameterTypes().at(0) == "Window" ) {
+				meth.invoke(obj, Q_ARG(int64_t, 1));
+			}
+		} else {
+			qDebug() << "Skipping" << meth.methodSignature();
+		}
+	}
+
+}
+
+QTEST_MAIN(TestCallAllMethods)
+#include "tst_callallmethods.moc"
