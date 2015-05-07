84f20cbd853be0f1e9d3803a1706399db8aea26a
Rui Abreu Ferreira <raf-ep@gmx.com>
2015-05-07 14:50:59 +0100

    tests: NeovimConnector::encode/decode and isReady

--- test/tst_neovimconnector.cpp
+++ test/tst_neovimconnector.cpp
@@ -6,6 +6,7 @@
 #include <QBuffer>
 
 #include <neovimconnector.h>
+#include "common.h"
 
 namespace NeovimQt {
 
@@ -24,6 +25,32 @@ private slots:
 		spawned->reconnect();
 	}
 
+	void isReady() {
+
+		NeovimConnector *c = NeovimConnector::spawn();
+		QSignalSpy onReady(c, SIGNAL(ready()));
+		QVERIFY(onReady.isValid());
+
+		QVERIFY(SPYWAIT(onReady));
+		QVERIFY(c->isReady());
+	}
+
+	void encodeDecode() {
+		NeovimConnector *c = NeovimConnector::spawn();
+
+		// This will print a warning, but should succeed
+		QString s = "ç日本語";
+		QByteArray bytes = c->encode(s);
+		QCOMPARE(c->decode(bytes), s);
+
+		QSignalSpy onReady(c, SIGNAL(ready()));
+		QVERIFY(onReady.isValid());
+		QVERIFY(SPYWAIT(onReady));
+
+		bytes = c->encode(s);
+		QCOMPARE(c->decode(bytes), s);
+
+	}
 };
 
 } // Namespace NeovimQt
