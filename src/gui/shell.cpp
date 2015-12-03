equalsraf/neovim-qt

commit 9ba51680e28376b3f58aae7489c8f8a3ad4d8400

GUI: Set the UI font
Accept notifications to set the GUI font from Neovim the notification name
name is "Gui", with the first argument "SetFont" followed by 0 or 1 additional
argument (the font name/size)

If not font name is passed it displays the current font, otherwise it sets the
current font e.g. "Monaco:h12" for 12pt Monaco. This follows the Vim font names
in Windows/Mac).

A command to set the font can be defined in nvimrc:

    :command -nargs=? Guifont call rpcnotify(0, 'Gui', 'SetFont', "<args>")

i.e. :Guifont Monaco:h12 can then be used to set the GUI font

When displaying the current font neovim-qt may print a different font than the
user set - this happens due to system font substitution, because it always
displays the actual font being used.

Fonts that do not exist, or are not fixed width cause an error message to
be displayed.

src/gui/shell.cpp
@@ -20,17 +20,7 @@ Shell::Shell(NeovimConnector *nvim, QWidget *parent)
 	m_resizing(false), m_logo(QPixmap(":/neovim.png")),
 	m_neovimBusy(false)
 {
-	QFont f;
-	f.setStyleStrategy(QFont::StyleStrategy(QFont::PreferDefault | QFont::ForceIntegerMetrics) );
-	f.setStyleHint(QFont::TypeWriter);
-	f.setFamily("DejaVu Sans Mono");
-	f.setFixedPitch(true);
-	f.setPointSize(11);
-	f.setKerning(false);
-	f.setFixedPitch(true);
-
-	m_font = f;
-	m_fm = new QFontMetrics(m_font);
+	setGuiFont("DejaVu Sans Mono:h11");
 
 	m_image = QImage(neovimSize(), QImage::Format_ARGB32_Premultiplied);
 
@@ -68,6 +58,64 @@ Shell::Shell(NeovimConnector *nvim, QWidget *parent)
 		neovimIsReady();
 	}
 }
+
+/**
+ * Set the GUI font, or display the current font
+ */
+bool Shell::setGuiFont(const QString& fdesc)
+{
+	if (fdesc.isEmpty()) {
+		QFontInfo fi(m_font);
+		QByteArray desc = m_nvim->encode(QString("%1:h%2\n").arg(fi.family()).arg(m_font.pointSize()));
+		m_nvim->neovimObject()->vim_out_write(desc);
+		return true;
+	}
+	QStringList attrs = fdesc.split(':');
+	if (attrs.size() < 1) {
+		m_nvim->neovimObject()->vim_report_error("Invalid font");
+		return false;
+	}
+
+	QFont f;
+	f.setStyleStrategy(QFont::StyleStrategy(QFont::PreferDefault | QFont::ForceIntegerMetrics) );
+	f.setStyleHint(QFont::TypeWriter);
+	f.setFamily(attrs.at(0));
+	f.setFixedPitch(true);
+	f.setKerning(false);
+	f.setFixedPitch(true);
+
+	foreach(QString attr, attrs) {
+		if (attr.size() >= 2 && attr[0] == 'h') {
+			bool ok = false;
+			int height = attr.mid(1).toInt(&ok);
+			if (!ok) {
+				m_nvim->neovimObject()->vim_report_error("Invalid font height");
+				return false;
+			}
+			f.setPointSize(height);
+		}
+	}
+
+	QFontInfo fi(f);
+	if (fi.family().compare(f.family(), Qt::CaseInsensitive) != 0 &&
+			f.family().compare("Monospace", Qt::CaseInsensitive) != 0) {
+		m_nvim->neovimObject()->vim_report_error("Unknown font");
+		return false;
+	}
+	if ( !fi.fixedPitch() ) {
+		m_nvim->neovimObject()->vim_report_error("Font needs to be fixed pitch");
+		return false;
+	}
+
+	m_font = f;
+	m_fm = new QFontMetrics(m_font);
+
+	if (m_attached) {
+		resizeNeovim(size());
+	}
+	return true;
+}
+
 
 Shell::~Shell()
 {
@@ -157,6 +205,9 @@ void Shell::neovimIsReady()
 
 	connect(m_nvim->neovimObject(), &Neovim::on_ui_try_resize,
 			this, &Shell::neovimResizeFinished);
+
+	// Subscribe to GUI events
+	m_nvim->neovimObject()->vim_subscribe("Gui");
 }
 
 void Shell::neovimError(NeovimConnector::NeovimError err)
@@ -480,7 +531,14 @@ void Shell::handleBusy(bool busy)
 // FIXME: fix QVariant type conversions
 void Shell::handleNeovimNotification(const QByteArray &name, const QVariantList& args)
 {
-	if (name != "redraw") {
+	if (name == "Gui" && args.size() > 0) {
+		QString guiEvName = m_nvim->decode(args.at(0).toByteArray());
+		if (guiEvName == "SetFont" && args.size() == 2) {
+			QString fdesc = m_nvim->decode(args.at(1).toByteArray());
+			setGuiFont(fdesc);
+		}
+		return;
+	} else if (name != "redraw") {
 		return;
 	}
 

src/gui/shell.h
@@ -32,6 +32,7 @@ signals:
 public slots:
 	void handleNeovimNotification(const QByteArray &name, const QVariantList& args);
 	void resizeNeovim(const QSize&);
+	bool setGuiFont(const QString& fdesc);
 
 protected slots:
 	void neovimIsReady();

commit 2d393d9973934e53f5a2874fb8043f0a2ae67c3d

Fix regression: initial screen was blank until user input was made
Commit 9ba51680e28376b3f58aae7489c8f8a3ad4d8400 introduced a bug that after the
application start you would only see a white screen with a broken cursor. It
would only disappear after some keyboard input.

I assume the reason for that was that we received notifications for drawing
events while waiting for the font information. The commit introducing this bug
says we need to wait with attaching until we receive the font, it didn't say
we can't connect the other signal handlers already, so this is what I did here.

src/gui/shell.cpp
@@ -213,18 +213,29 @@ void Shell::neovimIsReady()
 		return;
 	}
 
+	init();
+
 	// Check g:Guifont for a font from user settings
 	MsgpackRequest *r = m_nvim->neovimObject()->vim_get_var("Guifont");
 	connect(r, &MsgpackRequest::finished,
 			this, &Shell::neovimFontVarOk);
 	connect(r, &MsgpackRequest::error,
-			this, &Shell::init);
+			this, &Shell::attach);
 }
 
 void Shell::neovimFontVarOk(quint32, Function::FunctionId, const QVariant& ret)
 {
 	setGuiFont(m_nvim->decode(ret.toByteArray()));
-	init();
+	attach();
+}
+
+void Shell::attach()
+{
+	// FIXME: Don't set this here, wait for return from ui_attach instead
+	setAttached(true);
+	// FIXME: this API will change
+	QRect screenRect = QApplication::desktop()->availableGeometry(this);
+	m_nvim->attachUi(screenRect.width()*0.66/neovimCellWidth(), screenRect.height()*0.66/neovimRowHeight());
 }
 
 /**
@@ -233,14 +244,8 @@ void Shell::neovimFontVarOk(quint32, Function::FunctionId, const QVariant& ret)
  */
 void Shell::init()
 {
-	// FIXME: Don't set this here, wait for return from ui_attach instead
-	setAttached(true);
-
 	connect(m_nvim->neovimObject(), &Neovim::neovimNotification,
 			this, &Shell::handleNeovimNotification);
-	// FIXME: this API will change
-	QRect screenRect = QApplication::desktop()->availableGeometry(this);
-	m_nvim->attachUi(screenRect.width()*0.66/neovimCellWidth(), screenRect.height()*0.66/neovimRowHeight());
 
 	connect(m_nvim->neovimObject(), &Neovim::on_ui_try_resize,
 			this, &Shell::neovimResizeFinished);

src/gui/shell.h
@@ -44,6 +44,7 @@ protected slots:
 	void mouseClickReset();
 	void mouseClickIncrement(Qt::MouseButton bt);
         void init();
+	void attach();
 
 protected:
 	void tooltip(const QString& text);