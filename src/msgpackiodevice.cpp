equalsraf/neovim-qt

commit 38644b9036cc910d27013643bc0d349cc2138d4b

Communicate with Neovim using stdin/stdout
An incoming change for Neovim is to start launching the UI process instead of
having the UI start the Neovim process. Communication takes place using
stdin/out - similar to :rpcstart or --embed in nvim.

This commit refactors neovim-qt to be able to use stdin/stdout as a channel for
msgpack-rpc.

- MsgpackIODevice was refactored to be able to use stdin/stdout instead of a
  QIODevice
  - New static build function MsgpackIODevice::fromStdinOut() creates the device
  - The isOpen() method was updated accordingly.
  - The static method msgpack_write_cb() was moved into two variations
    msgpack_write_to_stdout() and msgpack_write_to_dev()
  - Two new slots as alternatives to dataAvailable(), dataAvailableStdin() used
    with the StdinReader, and dataAvailableFd() used with QSocketNotifier.
  - For Windows a new class StdinReader uses a background thread to do blocking
    reads and pass the data back to MsgpackIODevice. In UNIX a QSocketNotifier is
    used.
- The NeovimConnector class gets a new ctor to get a MsgpackIODevice instead of
  a QIODevice, and a static builder fromStdinOut().
- The GUI has a new argument --embed to use stdin/stdout to communicate with
  Neovim
- Added MSVC define for CRT_NONSTD_NO_DEPRECATE to use C functions

src/msgpackiodevice.cpp
@@ -1,5 +1,15 @@
 #include <QAbstractSocket>
 #include <QTextCodec>
+#include <QSocketNotifier>
+
+// read/write
+#ifdef _WIN32
+# include <io.h>
+# include "stdinreader.h"
+#else
+# include <unistd.h>
+#endif
+
 #include "msgpackiodevice.h"
 #include "util.h"
 #include "msgpackrequest.h"
@@ -13,22 +23,45 @@ namespace NeovimQt {
  *
  */
 
+/**
+ * Build a MsgpackIODevice that reads from stdin and writes to
+ * stdout
+ */
+MsgpackIODevice* MsgpackIODevice::fromStdinOut(QObject *parent)
+{
+	MsgpackIODevice *rpc = new MsgpackIODevice(NULL, parent);
+	msgpack_packer_init(&rpc->m_pk, rpc, (msgpack_packer_write)MsgpackIODevice::msgpack_write_to_stdout);
+#ifdef _WIN32
+	StdinReader *rsn = new StdinReader(msgpack_unpacker_buffer_capacity(&rpc->m_uk), rpc);
+	connect(rsn, &StdinReader::dataAvailable,
+			rpc, &MsgpackIODevice::dataAvailableStdin);
+	rsn->start();
+#else
+	QSocketNotifier *rsn = new QSocketNotifier(0, QSocketNotifier::Read, rpc);
+	connect(rsn, &QSocketNotifier::activated,
+			rpc, &MsgpackIODevice::dataAvailableFd);
+#endif
+	return rpc;
+}
+
 MsgpackIODevice::MsgpackIODevice(QIODevice *dev, QObject *parent)
 :QObject(parent), m_reqid(0), m_dev(dev), m_encoding(0), m_reqHandler(0), m_error(NoError)
 {
 	qRegisterMetaType<MsgpackError>("MsgpackError");
-	m_dev->setParent(this);
-
-	// MSVC: wont build without the (mspack_packer_write) cast
-	msgpack_packer_init(&m_pk, this, (msgpack_packer_write)MsgpackIODevice::msgpack_write_cb);
 	msgpack_unpacker_init(&m_uk, MSGPACK_UNPACKER_INIT_BUFFER_SIZE);
 
-	connect(m_dev, &QAbstractSocket::readyRead,
-			this, &MsgpackIODevice::dataAvailable);
+	if (m_dev) {
+		// MSVC: wont build without the (mspack_packer_write) cast
+		msgpack_packer_init(&m_pk, this, (msgpack_packer_write)MsgpackIODevice::msgpack_write_to_dev);
 
-	if ( !m_dev->isSequential() ) {
-		setError(InvalidDevice, tr("IO device needs to be sequential"));
-		return;
+		m_dev->setParent(this);
+		connect(m_dev, &QAbstractSocket::readyRead,
+				this, &MsgpackIODevice::dataAvailable);
+
+		if ( !m_dev->isSequential() ) {
+			setError(InvalidDevice, tr("IO device needs to be sequential"));
+			return;
+		}
 	}
 }
 
@@ -36,6 +69,17 @@ MsgpackIODevice::~MsgpackIODevice()
 {
 	//msgpack_packer_destroy(&m_pk);
 	msgpack_unpacker_destroy(&m_uk);
+}
+
+bool MsgpackIODevice::isOpen()
+{
+	if (m_error == InvalidDevice) {
+		return false;
+	} else if (m_dev) {
+		return m_dev->isOpen();
+	} else {
+		return true;
+	}
 }
 
 /** The encoding used by the MsgpackIODevice::encode and MsgpackIODevice::decode methods @see setEncoding */
@@ -63,39 +107,85 @@ bool MsgpackIODevice::setEncoding(const QByteArray& name)
 	return true;
 }
 
-/**
- * Called when the msgpack packer has data to write
- */
-int MsgpackIODevice::msgpack_write_cb(void* data, const char* buf, unsigned long int len)
+int MsgpackIODevice::msgpack_write_to_stdout(void* data, const char* buf, unsigned long int len)
 {
 	MsgpackIODevice *c = static_cast<MsgpackIODevice*>(data);
-	return c->m_dev->write(buf, len);
+	qint64 bytes = write(1, buf, len);
+	if (bytes == -1) {
+		c->setError(InvalidDevice, tr("Error writing to device"));
+	}
+	return bytes;
+}
+
+int MsgpackIODevice::msgpack_write_to_dev(void* data, const char* buf, unsigned long int len)
+{
+	MsgpackIODevice *c = static_cast<MsgpackIODevice*>(data);
+	qint64 bytes = c->m_dev->write(buf, len);
+	if (bytes == -1) {
+		c->setError(InvalidDevice, tr("Error writing to device"));
+	}
+	return bytes;
 }
 
 /**
- * Called when new data is available to be parsed
+ * Process incoming data.
+ *
+ * \see fromStdinOut() and StdinReader()
+ */
+void MsgpackIODevice::dataAvailableStdin(const QByteArray& data)
+{
+	if ( (quint64)data.length() > msgpack_unpacker_buffer_capacity(&m_uk)) {
+		setError(InvalidDevice, tr("Error when reading from stdin, BUG(buffered data exceeds capaciy)"));
+		return;
+	} else if ( data.length() > 0 ) {
+		memcpy(msgpack_unpacker_buffer(&m_uk), data.constData(), data.length());
+		msgpack_unpacker_buffer_consumed(&m_uk, data.length());
+		msgpack_unpacked result;
+		msgpack_unpacked_init(&result);
+		while(msgpack_unpacker_next(&m_uk, &result)) {
+			dispatch(result.data);
+		}
+	}
+}
+
+/**
+ * Process incoming data from the given fd.
+ *
+ * \see fromStdinOut() and QSocketNotifier()
+ */
+void MsgpackIODevice::dataAvailableFd(int fd)
+{
+	qint64 bytes = read(fd, msgpack_unpacker_buffer(&m_uk),
+			msgpack_unpacker_buffer_capacity(&m_uk));
+	if (bytes > 0) {
+		msgpack_unpacker_buffer_consumed(&m_uk, bytes);
+		msgpack_unpacked result;
+		msgpack_unpacked_init(&result);
+		while(msgpack_unpacker_next(&m_uk, &result)) {
+			dispatch(result.data);
+		}
+	} else if (bytes == -1) {
+		setError(InvalidDevice, tr("Error when reading from device"));
+	}
+}
+
+/**
+ * Process incoming data from the underlying device
+ *
+ * \see QIODevice()
  */
 void MsgpackIODevice::dataAvailable()
 {
-	qint64 read = 1;
-	while (read > 0) {
-		if ( msgpack_unpacker_buffer_capacity(&m_uk) == 0 ) {
-			if ( !msgpack_unpacker_reserve_buffer(&m_uk, 8192 ) ) {
-				// FIXME: error out
-				qWarning() << "Could not allocate memory in unpack buffer";
-				return;
-			}
+	qint64 bytes = m_dev->read(msgpack_unpacker_buffer(&m_uk), msgpack_unpacker_buffer_capacity(&m_uk));
+	if ( bytes > 0 ) {
+		msgpack_unpacker_buffer_consumed(&m_uk, bytes);
+		msgpack_unpacked result;
+		msgpack_unpacked_init(&result);
+		while(msgpack_unpacker_next(&m_uk, &result)) {
+			dispatch(result.data);
 		}
-
-		read = m_dev->read(msgpack_unpacker_buffer(&m_uk), msgpack_unpacker_buffer_capacity(&m_uk));
-		if ( read > 0 ) {
-			msgpack_unpacker_buffer_consumed(&m_uk, read);
-			msgpack_unpacked result;
-			msgpack_unpacked_init(&result);
-			while(msgpack_unpacker_next(&m_uk, &result)) {
-				dispatch(result.data);
-			}
-		}
+	} else if (bytes == -1) {
+		setError(InvalidDevice, tr("Error when reading from device"));
 	}
 }
 
@@ -340,8 +430,10 @@ QString MsgpackIODevice::errorString() const
 {
 	if (m_error) {
 		return m_errorString;
-	} else {
+	} else if (m_dev) {
 		return m_dev->errorString();
+	} else {
+		return QString();
 	}
 }
 

CMakeLists.txt
@@ -54,6 +54,11 @@ foreach(CFGNAME ${CMAKE_CONFIGURATION_TYPES})
   set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY_${CFGNAME} ${CMAKE_BINARY_DIR}/lib)
 endforeach()
 
+if(MSVC)
+  # Allow use of deprecated function names in MSVC (read/write)
+  add_definitions(-D_CRT_NONSTDC_NO_DEPRECATE)
+endif()
+
 add_subdirectory(src)
 add_subdirectory(doc)
 enable_testing()

src/CMakeLists.txt
@@ -1,5 +1,9 @@
 
 set(NEOVIM_QT_SOURCES util.cpp neovimconnector.cpp neovimconnectorhelper.cpp function.cpp msgpackrequest.cpp auto/neovim.cpp msgpackiodevice.cpp)
+if(WIN32)
+  list(APPEND NEOVIM_QT_SOURCES stdinreader.cpp)
+endif()
+
 add_library(neovim-qt STATIC ${NEOVIM_QT_SOURCES})
 target_link_libraries(neovim-qt Qt5::Network ${MSGPACK_LIBRARIES})
 

src/gui/main.cpp
@@ -42,20 +42,24 @@ int main(int argc, char **argv)
 		qInstallMessageHandler(logger);
 	}
 
-	QString server;
 	QStringList args = app.arguments().mid(1);
-	int serverIdx = args.indexOf("--server");
-	if (serverIdx != -1 && args.size() > serverIdx+1) {
-		server = args.at(serverIdx+1);
-		args.removeAt(serverIdx);
-		args.removeAt(serverIdx);
-	}
-
 	NeovimQt::NeovimConnector *c;
-	if (!server.isEmpty()) {
-		c = NeovimQt::NeovimConnector::connectToNeovim(server);
+	if (args.indexOf("--embed") != -1) {
+		c = NeovimQt::NeovimConnector::fromStdinOut();
 	} else {
-		c = NeovimQt::NeovimConnector::spawn(args);
+		QString server;
+		int serverIdx = args.indexOf("--server");
+		if (serverIdx != -1 && args.size() > serverIdx+1) {
+			server = args.at(serverIdx+1);
+			args.removeAt(serverIdx);
+			args.removeAt(serverIdx);
+		}
+
+		if (!server.isEmpty()) {
+			c = NeovimQt::NeovimConnector::connectToNeovim(server);
+		} else {
+			c = NeovimQt::NeovimConnector::spawn(args);
+		}
 	}
 
 #ifdef NEOVIMQT_GUI_WIDGET

src/msgpackiodevice.h
@@ -24,9 +24,9 @@ public:
 	};
 	MsgpackIODevice(QIODevice *, QObject *parent=0);
 	~MsgpackIODevice();
+        static MsgpackIODevice* fromStdinOut(QObject *parent=0);
 
-	/** True if the underlying IO device is open @see QIODevice::isOpen */
-	bool isOpen() {return m_dev->isOpen();}
+	bool isOpen();
 	QString errorString() const;
 	MsgpackError errorCause() const {return m_error;};
 
@@ -86,11 +86,16 @@ protected:
 
 protected slots:
 	void setError(MsgpackError err, const QString& msg);
+
 	void dataAvailable();
+	void dataAvailableStdin(const QByteArray&);
+	void dataAvailableFd(int fd);
+
 	void requestTimeout(quint32 id);
 
 private:
-	static int msgpack_write_cb(void* data, const char* buf, unsigned long int len);
+	static int msgpack_write_to_stdout(void* data, const char* buf, unsigned long int len);
+	static int msgpack_write_to_dev(void* data, const char* buf, unsigned long int len);
 
 	quint32 m_reqid;
 	QIODevice *m_dev;

src/neovimconnector.cpp
@@ -20,11 +20,15 @@ namespace NeovimQt {
  * Create a new Neovim API connection from an open IO device
  */
 NeovimConnector::NeovimConnector(QIODevice *dev)
-:QObject(), m_dev(0), m_helper(0), m_error(NoError), m_neovimobj(NULL), 
+:NeovimConnector(new MsgpackIODevice(dev))
+{
+}
+
+NeovimConnector::NeovimConnector(MsgpackIODevice *dev)
+:QObject(), m_dev(dev), m_helper(0), m_error(NoError), m_neovimobj(NULL),
 	m_channel(0), m_ctype(OtherConnection), m_ready(false)
 {
 	m_helper = new NeovimConnectorHelper(this);
-	m_dev = new MsgpackIODevice(dev, this);
 	qRegisterMetaType<NeovimError>("NeovimError");
 
 	connect(m_dev, &MsgpackIODevice::error,
@@ -258,6 +262,11 @@ NeovimConnector* NeovimConnector::connectToNeovim(const QString& server)
 		}
 	}
 	return connectToSocket(addr);
+}
+
+NeovimConnector* NeovimConnector::fromStdinOut()
+{
+	return new NeovimConnector(MsgpackIODevice::fromStdinOut());
 }
 
 /**

src/neovimconnector.h
@@ -47,10 +47,12 @@ public:
         };
 
 	NeovimConnector(QIODevice* s);
+	NeovimConnector(MsgpackIODevice* s);
 	static NeovimConnector* spawn(const QStringList& params=QStringList());
 	static NeovimConnector* connectToSocket(const QString&);
 	static NeovimConnector* connectToHost(const QString& host, int port);
 	static NeovimConnector* connectToNeovim(const QString& server=QString());
+	static NeovimConnector* fromStdinOut();
 
 	bool canReconnect();
 	NeovimConnector* reconnect();

src/stdinreader.cpp
@@ -1,0 +1,59 @@
+#include "stdinreader.h"
+
+#include <QDebug>
+#ifdef _WIN32
+# include <io.h>
+# include <fcntl.h>
+#else
+# include <unistd.h>
+#endif
+
+namespace NeovimQt {
+
+/**
+ * \class NeovimQt::StdinReader
+ *
+ * A background thread to read data from Stdin
+ *
+ * For Unix systems a better alternative to this is to use QSocketNotifier, with
+ * a little work it produces the same effect with none of the overhead.
+ */
+
+/**
+ * Read from stdin in a background thread with
+ * @arg maxSize is the read buffer size
+ */
+StdinReader::StdinReader(qint64 maxSize, QObject *parent)
+:QThread(parent), m_maxSize(maxSize)
+{
+#ifdef _WIN32
+	setmode(0, _O_BINARY);
+#endif
+	if (!m_in.open(0, QIODevice::ReadOnly | QIODevice::Unbuffered)) {
+		qWarning("Unable to open stdin for reading");
+	}
+}
+
+void StdinReader::run()
+{
+	char *buf = new char[m_maxSize];
+	while (true) {
+		qint64 bytes = read(0, buf, m_maxSize);
+		if (bytes > 0 ) {
+			qDebug() << "Reading data from Stdin" << bytes;
+			emit dataAvailable(QByteArray(buf, bytes));
+		}
+	}
+	delete buf;
+}
+
+/**
+ * \fn NeovimQt::StdinReader::dataAvailable()
+ *
+ * Emitted when data is read from stdin
+ *
+ * @arg data is no larger than maxSize
+ * @see StdinReader::StdinReader
+ */
+
+} // Namespace

commit d199d7f8383673c4a5e2f07854e69cc7efc2dd73

Fix regression: Revert MsgpackIODevice::dataAvailable()
Commit 38644b9036cc910d27013643bc0d349cc2138d4b broke this function by removing
the buffer reserving and the loop to read data, causing nvim-qt to freeze after
a certain amount of packets.

src/msgpackiodevice.cpp
@@ -176,16 +176,25 @@ void MsgpackIODevice::dataAvailableFd(int fd)
  */
 void MsgpackIODevice::dataAvailable()
 {
-	qint64 bytes = m_dev->read(msgpack_unpacker_buffer(&m_uk), msgpack_unpacker_buffer_capacity(&m_uk));
-	if ( bytes > 0 ) {
-		msgpack_unpacker_buffer_consumed(&m_uk, bytes);
-		msgpack_unpacked result;
-		msgpack_unpacked_init(&result);
-		while(msgpack_unpacker_next(&m_uk, &result)) {
-			dispatch(result.data);
+	qint64 read = 1;
+	while (read > 0) {
+		if ( msgpack_unpacker_buffer_capacity(&m_uk) == 0 ) {
+			if ( !msgpack_unpacker_reserve_buffer(&m_uk, 8192 ) ) {
+				// FIXME: error out
+				qWarning() << "Could not allocate memory in unpack buffer";
+				return;
+			}
 		}
-	} else if (bytes == -1) {
-		setError(InvalidDevice, tr("Error when reading from device"));
+
+		read = m_dev->read(msgpack_unpacker_buffer(&m_uk), msgpack_unpacker_buffer_capacity(&m_uk));
+		if ( read > 0 ) {
+			msgpack_unpacker_buffer_consumed(&m_uk, read);
+			msgpack_unpacked result;
+			msgpack_unpacked_init(&result);
+			while(msgpack_unpacker_next(&m_uk, &result)) {
+				dispatch(result.data);
+			}
+		}
 	}
 }
 