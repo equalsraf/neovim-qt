4927d14ba6a47bd1efe4659c635377254b5e5da3
Rui Abreu Ferreira <raf-ep@gmx.com>
2016-01-28 13:49:56 +0000

    Added new tests for input handling

---
+++ test/tst_input.cpp
@@ -0,0 +1,44 @@
+
+#include <QtTest/QtTest>
+#include <gui/input.h>
+
+class TestInput: public QObject
+{
+	Q_OBJECT
+private slots:
+	void specialKeys();
+private:
+	NeovimQt::InputConv input;
+};
+
+void TestInput::specialKeys()
+{
+	foreach(int k, input.specialKeys.keys()) {
+		QCOMPARE(input.convertKey("", k, Qt::NoModifier),
+			QString("<%1>").arg(input.specialKeys.value(k)));
+
+		QCOMPARE(input.convertKey("", k, Qt::ControlModifier),
+#ifdef Q_OS_MAC
+			// On Mac Control is actually the Cmd key, which we
+			// don't support yet
+			QString("<%1>").arg(input.specialKeys.value(k)));
+#else
+			QString("<C-%1>").arg(input.specialKeys.value(k)));
+#endif
+
+		QCOMPARE(input.convertKey("", k, Qt::AltModifier),
+			QString("<A-%1>").arg(input.specialKeys.value(k)));
+
+		QCOMPARE(input.convertKey("", k, Qt::MetaModifier),
+#ifdef Q_OS_MAC
+			// On Mac Meta is actually the Control key
+			QString("<C-%1>").arg(input.specialKeys.value(k)));
+#else
+			// Meta is not handled right now
+			QString("<%1>").arg(input.specialKeys.value(k)));
+#endif
+	}
+}
+
+QTEST_MAIN(TestInput)
+#include "tst_input.moc"
