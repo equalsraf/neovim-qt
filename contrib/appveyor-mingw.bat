6a4ac22ca070815e8297a9063b0b90699e333a40
Rui Abreu Ferreira <raf-ep@gmx.com>
2016-02-18 01:01:41 +0000

    Appveyor: Cleanup settings, add Qt 5.5/Mingw build
    - Cleanup old settings in Appveyor that are no longer used since the
    Nuget packages were removed.
    - Bump Qt version to 5.5.
    - Add MinGW builds.

---
+++ contrib/appveyor-mingw.bat
@@ -0,0 +1,9 @@
+rem CMake/MinGW workaround - remove sh.exe from PATH
+where sh
+set PATH=%PATH:C:\Program Files\Git\usr\bin;=%
+set CMAKE_PREFIX_PATH=%QTDIR%
+set PATH=%MINGW%\bin;%PATH%;%QTDIR%\bin
+mkdir build
+cd build
+cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=%configuration% -DCMAKE_INSTALL_PREFIX=../INSTALL ..
+cmake --build . --target windeployqt
---
+++ contrib/appveyor-msvc.bat
@@ -0,0 +1,7 @@
+set CMAKE_PREFIX_PATH=%QTDIR%
+set PATH=%PATH%;%QTDIR%\bin
+mkdir build
+cd build
+rem FYI CMAKE_BUILD_TYPE does not work with VS generators, use --config
+cmake -G "Visual Studio 12" -DCMAKE_BUILD_TYPE=%configuration% -DCMAKE_INSTALL_PREFIX=../INSTALL ..
+cmake --build . --config %configuration% --target windeployqt
