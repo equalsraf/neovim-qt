c59a121da95f73e57beb0e194248a8b6300d9a91
Rui Abreu Ferreira <raf-ep@gmx.com>
2016-07-02 20:00:53 +0100

    GUI: Use icons in popup widget

--- src/gui/popupwidgetitem.cpp
+++ src/gui/popupwidgetitem.cpp
@@ -8,6 +8,14 @@ PopupWidgetItem::PopupWidgetItem(const QString& text,
 		const QString& kind, const QString& extra, const QString& info)
 :QListWidgetItem(text)
 {
+	qDebug() << extra;
+	if (extra.trimmed() == "[F]") {
+		setIcon(QIcon(":/icons/inode-directory.png"));
+	} else if (extra.trimmed() == "[B]") {
+		setIcon(QIcon(":/icons/document-open-recent.png"));
+	} else {
+		setIcon(QIcon(":/icons/unknown.png"));
+	}
 }
 
 } // Namespace
