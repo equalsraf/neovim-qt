56905ad2b0151243cab460a818d4d8ebf78fd5c3
Rui Abreu Ferreira <raf-ep@gmx.com>
2016-08-30 17:11:07 +0100

    Bump Qt version in appveyor builds to 5.7

--- contrib/appveyor-mingw.bat
+++ contrib/appveyor-mingw.bat
@@ -1,9 +1,11 @@
 rem CMake/MinGW workaround - remove sh.exe from PATH
 where sh
+set MINGW=C:\Qt\Tools\mingw530_32
+set QTDIR=C:\Qt\5.7\mingw53_32
 set PATH=%PATH:C:\Program Files\Git\usr\bin;=%
 set CMAKE_PREFIX_PATH=%QTDIR%
 set PATH=%MINGW%\bin;%PATH%;%QTDIR%\bin
 mkdir build
 cd build
-cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=%configuration% -DCMAKE_INSTALL_PREFIX=../INSTALL ..
+cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../INSTALL ..
 cmake --build . --target install
