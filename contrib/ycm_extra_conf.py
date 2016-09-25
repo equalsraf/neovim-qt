d17ae129ffd2108b40020b178b870a1ba5c2e7ec
Matthieu Coudron <mattator@gmail.com>
2016-09-25 15:37:33 +0200

    Adds YouCompleteMe support
    cmake now generates a compile_commands.json that can be leveraged by
    YouCompleteMe, chromatica etc...
    You can for instance `ln -s contrib/ycm_extra_conf.py
    .ycm_extra_conf.py`

---
+++ contrib/ycm_extra_conf.py
@@ -0,0 +1,54 @@
+# .ycm_extra_conf.py for nvim source code.
+import os
+import ycm_core
+
+
+def DirectoryOfThisScript():
+    return os.path.dirname(os.path.abspath(__file__))
+
+
+def GetDatabase():
+    compilation_database_folder = DirectoryOfThisScript()
+    if os.path.exists(compilation_database_folder):
+        return ycm_core.CompilationDatabase(compilation_database_folder)
+    return None
+
+
+def IsHeaderFile(filename):
+    extension = os.path.splitext(filename)[1]
+    return extension == '.h'
+
+
+def GetCompilationInfoForFile(filename):
+    database = GetDatabase()
+    if not database:
+        return None
+    if IsHeaderFile(filename):
+        basename = os.path.splitext(filename)[0]
+        c_file = basename + '.c'
+        # for pure headers (no c file), default to main.c
+        if not os.path.exists(c_file):
+            c_file = os.path.join(DirectoryOfThisScript(), 'nvim', 'main.c')
+        if os.path.exists(c_file):
+            compilation_info = database.GetCompilationInfoForFile(c_file)
+            if compilation_info.compiler_flags_:
+                return compilation_info
+        return None
+    return database.GetCompilationInfoForFile(filename)
+
+
+def FlagsForFile(filename):
+    compilation_info = GetCompilationInfoForFile(filename)
+    if not compilation_info:
+        return None
+    # Add flags not needed for clang-the-binary,
+    # but needed for libclang-the-library (YCM uses this last one).
+    flags = (list(compilation_info.compiler_flags_)
+             if compilation_info.compiler_flags_
+             else [])
+    extra_flags = ['-Wno-newline-eof']
+    final_flags = flags + extra_flags
+    return {
+        'flags': final_flags,
+        'do_cache': True
+    }
