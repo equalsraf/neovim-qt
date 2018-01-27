6be2055cca8e33a6c36a8c919193f1ce206b1717
Rui Abreu Ferreira <rap-ep@gmx.com>
2018-01-27 21:11:38 +0000

    Enable tests in appveyor
    Tests are still disabled for MSVC and Qt53, I left the commented code in, but
    they fail due to an exception e.g.
    
         1/12 Test  #1: tst_neovimobject .................***Exception: Exit code 0xc0000139
    
    the exit code seems to correspond to failure to load a library.

--- contrib/appveyor-mingw.bat
+++ contrib/appveyor-mingw.bat
@@ -5,8 +5,10 @@ set QTDIR=C:\Qt\5.7\mingw53_32
 set PATH=%PATH:C:\Program Files\Git\usr\bin;=%
 set CMAKE_PREFIX_PATH=%QTDIR%
 set PATH=%MINGW%\bin;%PATH%;%QTDIR%\bin
+:: Qt needs to find the windows platform plugin to run tests
+set QT_QPA_PLATFORM_PLUGIN_PATH=%QTDIR%\plugins
 mkdir build
 cd build
 cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../INSTALL ..
 cmake --build . --target install
-rem ctest -VV
+ctest -VV
--- contrib/appveyor-msvc.bat
+++ contrib/appveyor-msvc.bat
@@ -3,6 +3,9 @@ set CMAKE_PREFIX_PATH=%QTDIR%
 set PATH=%PATH%;%QTDIR%\bin
 mkdir build
 cd build
-rem FYI CMAKE_BUILD_TYPE does not work with VS generators, use --config
+:: FYI CMAKE_BUILD_TYPE does not work with VS generators, use --config
 cmake -G "Visual Studio 14" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../INSTALL ..
 cmake --build . --config Release --target install
+:: TODO enable tests for msvc builds
+:: set QT_QPA_PLATFORM_PLUGIN_PATH=%QTDIR%\plugins
+:: ctest -C Release -VV
