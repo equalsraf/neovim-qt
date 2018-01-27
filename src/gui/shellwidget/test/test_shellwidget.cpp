ccfe745529764f72348463aaca123fb821ac533b
George Zhao <zhaozg@gmail.com>
2018-01-27 11:47:01 +0800

    Make mingw32-make test on windows when qt5-static supported

--- src/gui/shellwidget/test/test_shellwidget.cpp
+++ src/gui/shellwidget/test/test_shellwidget.cpp
@@ -1,6 +1,11 @@
 #include <QtTest/QtTest>
 #include "shellwidget.h"
 
+#if defined(Q_OS_WIN) && defined(USE_STATIC_QT)
+#include <QtPlugin>
+Q_IMPORT_PLUGIN (QWindowsIntegrationPlugin);
+#endif
+
 class Test: public QObject
 {
 	Q_OBJECT
--- test/tst_shell.cpp
+++ test/tst_shell.cpp
@@ -5,6 +5,11 @@
 #include <msgpackrequest.h>
 #include "common.h"
 
+#if defined(Q_OS_WIN) && defined(USE_STATIC_QT)
+#include <QtPlugin>
+Q_IMPORT_PLUGIN (QWindowsIntegrationPlugin);
+#endif
+
 namespace NeovimQt {
 
 class Test: public QObject
