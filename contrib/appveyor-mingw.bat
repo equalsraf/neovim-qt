1edbfeaf44d2197b6710ea5447649c685ddd5db6
Rui Abreu Ferreira <raf-ep@gmx.com>
2018-06-20 14:56:14 +0100

    Appveyor: Bump Qt/MSVC version to 5.11

--- contrib/appveyor-mingw.bat
+++ contrib/appveyor-mingw.bat
@@ -1,7 +1,7 @@
 rem CMake/MinGW workaround - remove sh.exe from PATH
 where sh
 set MINGW=C:\Qt\Tools\mingw530_32
-set QTDIR=C:\Qt\5.7\mingw53_32
+set QTDIR=C:\Qt\5.11.0\mingw53_32
 set PATH=%PATH:C:\Program Files\Git\usr\bin;=%
 set CMAKE_PREFIX_PATH=%QTDIR%
 set PATH=%MINGW%\bin;%PATH%;%QTDIR%\bin
