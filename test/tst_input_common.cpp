76d1e756df02adba462770240d47d5b3484a9670
John Gehrig <jdg.gehrig@gmail.com>
2019-12-07 17:37:05 -0500

    Issue 593 Control + Super inserts ^S
    Some versions of Qt will send key events using Key_Super_L and Key_Super_R. We
    should handle these keys like other modifier keys. If the event contains only
    modifier keys it should be ignored.
    
    UTs added for ignoring various modifier key combination, as well as this
    specific scenario.

--- test/tst_input_common.cpp
+++ test/tst_input_common.cpp
@@ -20,6 +20,38 @@ void TestInputCommon::ModifierOnlyKeyEventsIgnored() noexcept
 
 	QKeyEvent alt{ QEvent::KeyPress, Qt::Key_Alt, Qt::AltModifier, {} };
 	QCOMPARE(NeovimQt::Input::convertKey(alt), QString{});
+
+	const QList<Qt::Key> modifierKeyList {
+		Qt::Key::Key_Super_L,
+		Qt::Key::Key_Super_R,
+		Qt::Key::Key_Control,
+		Qt::Key::Key_Meta,
+		Qt::Key::Key_Alt,
+		Qt::Key::Key_Shift,
+	};
+
+	const QList<Qt::KeyboardModifier> keyboardModifierList {
+		Qt::KeyboardModifier::NoModifier,
+		Qt::KeyboardModifier::ShiftModifier,
+		Qt::KeyboardModifier::ControlModifier,
+		Qt::KeyboardModifier::AltModifier,
+		Qt::KeyboardModifier::MetaModifier,
+	};
+
+	for (const auto& key : modifierKeyList) {
+		for (const auto& mod : keyboardModifierList) {
+			QKeyEvent keyEvent{ QEvent::KeyPress, key, mod };
+			QCOMPARE(NeovimQt::Input::convertKey(keyEvent), QString{});
+		}
+	}
+
+	// Issue#344: <C-S-> appears as <C-Space>
+	QKeyEvent evIssue344{ QEvent::KeyPress, Qt::Key::Key_Control, Qt::KeyboardModifier::ShiftModifier};
+	QCOMPARE(NeovimQt::Input::convertKey(evIssue344), QString{});
+
+	// Issue#593: Pressing Control + Super inserts ^S
+	QKeyEvent evIssue593{ QEvent::KeyPress, Qt::Key::Key_Super_L, Qt::KeyboardModifier::ControlModifier};
+	QCOMPARE(NeovimQt::Input::convertKey(evIssue593), QString{});
 }
 
 #include "tst_input_common.moc"
