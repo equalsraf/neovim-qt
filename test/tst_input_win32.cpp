548bb1e09aa339571ca4cc8625529fcdc37ceb10
John Gehrig <jdg.gehrig@gmail.com>
2019-12-07 14:14:15 -0500

    Refactor input handling logic and improve UnitTest coverage
    Several outstanding bugs are related to the way key input is handled. This
    code is difficult to modify and validate due to the usage of #ifdefs. Each
    platform has different behavior, and UTs only validate the active platform.
    
    This change does the following:
    1) Get rid of all #ifdefs, to make the code easier to read.
    2) Split platform specific behavior into separate compilation units.
    3) Add UT coverage for all platforms, regardless of active platform.
    4) Remove unused logic.

---
+++ test/tst_input_win32.cpp
@@ -0,0 +1,34 @@
+#include <QtTest/QtTest>
+
+#include <gui/input.h>
+
+class TestInputWin32 : public QObject
+{
+	Q_OBJECT
+
+private slots:
+	void SpecialKeys() noexcept;
+};
+
+void TestInputWin32::SpecialKeys() noexcept
+{
+	const QMap<int, QString>& specialKeys { NeovimQt::Input::GetSpecialKeysMap() };
+
+	for (const auto k : specialKeys.keys()) {
+		// On Mac Meta is the Control key, treated as C-.
+		QList<QPair<QKeyEvent, QString>> keyEventList{
+			{ { QEvent::KeyPress, k, Qt::NoModifier, {} },      "<%1>" },
+			{ { QEvent::KeyPress, k, Qt::ControlModifier, {} }, "<C-%1>" },
+			{ { QEvent::KeyPress, k, Qt::AltModifier, {} },     "<A-%1>" },
+			{ { QEvent::KeyPress, k, Qt::MetaModifier, {} },    "<%1>" },
+		};
+
+		for (const auto& keyEvent : keyEventList) {
+			QCOMPARE(NeovimQt::Input::convertKey(keyEvent.first),
+				keyEvent.second.arg(specialKeys.value(k)));
+		}
+	}
+}
+
+#include "tst_input_win32.moc"
+QTEST_MAIN(TestInputWin32)
