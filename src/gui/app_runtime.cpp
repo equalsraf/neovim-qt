1ca67f729827d82b9f3a7a3efcbe3a09e8a813cd
John Gehrig <jdg.gehrig@gmail.com>
2019-12-12 21:58:59 -0500

    Add runtime path and nvim args to --version
    Modularize the argument code handling code so it can be called and displayed
    in the version message. Removes several ifdefs.

---
+++ src/gui/app_runtime.cpp
@@ -0,0 +1,30 @@
+#include "app.h"
+
+#include <QDir>
+#include <QFileInfo>
+
+namespace NeovimQt {
+
+QString App::getRuntimePath() noexcept
+{
+	QString path{ QString::fromLocal8Bit(qgetenv("NVIM_QT_RUNTIME_PATH")) };
+
+	if (QFileInfo(path).isDir()) {
+		return path;
+	}
+
+	if (QFileInfo(NVIM_QT_RUNTIME_PATH).isDir()) {
+		return QStringLiteral(NVIM_QT_RUNTIME_PATH);
+	}
+
+	// Look for the runtime relative to the nvim-qt binary
+	const QDir d = QDir{ applicationDirPath() }.filePath("../share/nvim-qt/runtime");
+
+	if (d.exists()) {
+		return d.path();
+	}
+
+	return {};
+}
+
+} // namespace NeovimQt
