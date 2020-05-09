9b8821cb492c3523638b02d921fcb67669446e33
John Gehrig <jdg.gehrig@gmail.com>
2020-05-09 04:25:17 -0400

    Modularize font description to QFont function
    This functionality will be required by guifontwide.
    
    The guifontwide option must convert descriptions to QFont objects too.
    
    Adds unit tests, and `:w` range limits.

--- src/gui/shellwidget/test/test_shellwidget.cpp
+++ src/gui/shellwidget/test/test_shellwidget.cpp
@@ -13,6 +13,7 @@ class Test: public QObject
 private slots:
 	void clearRegion();
 	void fontDescriptionFromQFont();
+	void fontDescriptionToQFont();
 };
 
 
@@ -42,6 +43,87 @@ void Test::fontDescriptionFromQFont()
 	const int arbitraryWeight{ QFont::Weight::Normal + 1 };
 	QCOMPARE(ShellWidget::fontDesc({ fontFamily, 16, arbitraryWeight, true/*italic*/ }),
 		QString{ "%1:h16:w%2:i" }.arg(fontFamily).arg(arbitraryWeight));
+}
+
+static bool ValidateQFont(
+	const QVariant& varFont,
+	const QString& fontFamily,
+	qreal pointSizeF,
+	int weight,
+	bool italic) noexcept
+{
+	if (!varFont.canConvert<QFont>()) {
+		return false;
+	}
+
+	const QFont font{ qvariant_cast<QFont>(varFont) };
+
+	if (font.family() != fontFamily
+		|| font.pointSizeF() != pointSizeF
+		|| font.weight() != weight
+		|| font.italic() != italic) {
+		return false;
+	}
+
+	return true;
+}
+
+void Test::fontDescriptionToQFont()
+{
+	const QString fontFamily{ ShellWidget::getDefaultFontFamily() };
+	const QFont defaultFont{ fontFamily, 11 };
+
+	//
+	// Expected Cases, basic functionality
+	//
+	QVERIFY(ValidateQFont(
+		ShellWidget::TryGetQFontFromDescription(QString{ "%1:h11" }.arg(fontFamily), defaultFont),
+		fontFamily, 11, QFont::Weight::Normal, false /*italic*/));
+
+	QVERIFY(ValidateQFont(
+		ShellWidget::TryGetQFontFromDescription(QString{ "%1:h12:l" }.arg(fontFamily), defaultFont),
+		fontFamily, 12, QFont::Weight::Light, false /*italic*/));
+
+	QVERIFY(ValidateQFont(
+		ShellWidget::TryGetQFontFromDescription(QString{ "%1:h14:sb" }.arg(fontFamily), defaultFont),
+		fontFamily, 14, QFont::Weight::DemiBold, false /*italic*/));
+
+
+	QVERIFY(ValidateQFont(
+		ShellWidget::TryGetQFontFromDescription(QString{ "%1:h16:b" }.arg(fontFamily), defaultFont),
+		fontFamily, 16, QFont::Weight::Bold, false /*italic*/));
+
+	const int arbitraryWeight{ QFont::Weight::Normal + 1 };
+	QVERIFY(ValidateQFont(
+		ShellWidget::TryGetQFontFromDescription(QString{ "%1:h16:w%2" }.arg(fontFamily).arg(arbitraryWeight), defaultFont),
+		fontFamily, 16, arbitraryWeight, false /*italic*/));
+
+	//
+	// Error cases
+	//
+	const QVariant varInvalidLargeWeight{
+		ShellWidget::TryGetQFontFromDescription(QString{ "%1:h16:w%2" }.arg(fontFamily).arg(10000), defaultFont) };
+
+	QVERIFY(!ShellWidget::IsValidFont(varInvalidLargeWeight));
+	QCOMPARE(varInvalidLargeWeight, QVariant{ QString{ "Invalid font weight" } });
+
+	const QVariant varInvalidNegativeWeight{
+		ShellWidget::TryGetQFontFromDescription(QString{ "%1:h16:w%2" }.arg(fontFamily).arg(-1), defaultFont) };
+
+	QVERIFY(!ShellWidget::IsValidFont(varInvalidNegativeWeight));
+	QCOMPARE(varInvalidNegativeWeight, QVariant{ QString{ "Invalid font weight" } });
+
+	const QVariant varInvalidHeight{
+		ShellWidget::TryGetQFontFromDescription(QString{ "FooBarInvalidFont:ha" }, defaultFont) };
+
+	QVERIFY(!ShellWidget::IsValidFont(varInvalidHeight));
+	QCOMPARE(varInvalidHeight, QVariant{ QString{ "Invalid font height" } });
+
+	const QVariant varInvalidNegativeHeight{
+		ShellWidget::TryGetQFontFromDescription(QString{ "FooBarInvalidFont:h-1" }, defaultFont) };
+
+	QVERIFY(!ShellWidget::IsValidFont(varInvalidNegativeHeight));
+	QCOMPARE(varInvalidHeight, QVariant{ QString{ "Invalid font height" } });
 }
 
 QTEST_MAIN(Test)
