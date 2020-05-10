bd95f857005b3816fe633ca03a1d3cae844cf52b
equalsraf <redacted>
2020-05-10 19:48:05 +0100

    Add test for output rendering
    For now this is mostly an example - but it seems useful to have the
    ability to test rendering output.

--- src/gui/shellwidget/test/test_shellwidget.cpp
+++ src/gui/shellwidget/test/test_shellwidget.cpp
@@ -1,4 +1,6 @@
+#include <QFontDatabase>
 #include <QtTest/QtTest>
+#include <QPainter>
 #include "shellwidget.h"
 
 #if defined(Q_OS_WIN) && defined(USE_STATIC_QT)
@@ -14,8 +16,51 @@ private slots:
 	void clearRegion();
 	void fontDescriptionFromQFont();
 	void fontDescriptionToQFont();
+	void render();
+private:
+	QString outputFolderPath();
+	QString outputFilePath(const QString& name);
+	QString originalFilePath(const QString& name);
+	void saveWidgetOutput(ShellWidget& w, QString name);
+	void diffWidgetOutput(QString name);
+
+private slots:
+	void initTestCase() noexcept;
 };
 
+
+void Test::initTestCase() noexcept
+{
+	const QStringList fonts{
+		QStringLiteral("third-party/DejaVuSansMono.ttf"),
+		QStringLiteral("third-party/DejaVuSansMono-Bold.ttf"),
+		QStringLiteral("third-party/DejaVuSansMono-BoldOblique.ttf") };
+
+	for (const auto& path : fonts) {
+		QString abs_path_to_font(CMAKE_SOURCE_DIR);
+		abs_path_to_font.append("/").append(path);
+		QFontDatabase::addApplicationFont(abs_path_to_font);
+	}
+}
+
+QString Test::outputFolderPath()
+{
+	auto folderName = "test_shellwidget_output";
+	QDir().mkpath(folderName);
+	return folderName;
+}
+
+/// Render Output file path to save a file
+QString Test::outputFilePath(const QString& name)
+{
+	return QDir(outputFolderPath()).filePath(name);
+}
+
+/// Previous output from rendering, from the source test folder
+QString Test::originalFilePath(const QString& name)
+{
+	return QDir(QDir(TEST_SOURCE_DIR).filePath("renderoutput")).filePath(name);
+}
 
 void Test::clearRegion()
 {
@@ -124,6 +169,87 @@ void Test::fontDescriptionToQFont()
 
 	QVERIFY(!ShellWidget::IsValidFont(varInvalidNegativeHeight));
 	QCOMPARE(varInvalidHeight, QVariant{ QString{ "Invalid font height" } });
+}
+
+void Test::saveWidgetOutput(ShellWidget& w, QString name)
+{
+	QImage img(w.size(), QImage::Format_ARGB32);
+	QPainter painter(&img);
+	w.render(&painter);
+	QCOMPARE(img.save(outputFilePath(name)), true);
+}
+
+void Test::diffWidgetOutput(QString name)
+{
+	auto p1 = outputFilePath(name);
+	auto output = QImage(p1);
+	qDebug() << "Loading" << p1 << output;
+	auto p2 = originalFilePath(name);
+	auto expected = QImage(p2);
+	qDebug() << "Loading" << p2 << expected;
+
+	QCOMPARE(output.isNull(), false);
+	QCOMPARE(expected.isNull(), false);
+
+	QImage diff(output.size(), QImage::Format_RGB32);
+	diff.fill(Qt::white);
+
+	bool failed = false;
+
+	for (int y=0; y < output.height(); y++) {
+		for (int x=0; x < output.width(); x++) {
+			auto outputColor = output.pixel(x, y);
+			auto expectedColor = expected.pixel(x, y);
+
+			if (outputColor != expectedColor) {
+				//qWarning() << "Pixel color mismatch at position " << x << y;
+				//qWarning() << "  output:" << outputColor << "expected:" << expectedColor;
+				diff.setPixel(x, y, Qt::red);
+				failed = true;
+			}
+		}
+	}
+
+	auto outpath = outputFilePath("diff_" + name);
+	qDebug() << "Saving diff" << outpath;
+
+
+	if (QGuiApplication::platformName() != "xcb") {
+		auto msg = QString("Skipping render test in non X11: %1")
+			.arg(QGuiApplication::platformName());
+		QSKIP(qPrintable(msg));
+	}
+
+	QCOMPARE(diff.save(outpath), true);
+
+	QCOMPARE(output.width(), expected.width());
+	QCOMPARE(output.height(), expected.height());
+
+	if (failed) {
+		qWarning() << "Failing diff, check the output diff image" << outpath;
+		QCOMPARE(failed, false);
+	}
+}
+
+/// This is mostly an example it renders a shell widget and saves it as an image.
+/// For more complicated tests we may want to parametrize these.
+void Test::render()
+{
+	ShellWidget w;
+	w.setShellFont(QFont("DejaVu Sans Mono", 11, QFont::Weight::Normal, false));
+	w.resizeShell(20, 20);
+	w.show();
+
+	w.put("hello", 2, 2, HighlightAttribute());
+	w.put("fffffff", 3, 3, HighlightAttribute(Qt::red, Qt::black, Qt::white, false, false, false, false, false, false));
+	w.put("italic text", 4, 3, HighlightAttribute(Qt::white, Qt::black, Qt::white, false, true, false, false, false, false));
+	w.put("bold text", 5, 3, HighlightAttribute(Qt::white, Qt::black, Qt::white, false, false, true, false, false, false));
+	w.put("underline text", 6, 3, HighlightAttribute(Qt::white, Qt::black, Qt::white, false, false, false, true, false, false));
+	w.put("undercurl text", 7, 3, HighlightAttribute(Qt::white, Qt::black, Qt::red, false, false, false, false, true, false));
+
+	auto name = "shellwidget_render_works.png";
+	saveWidgetOutput(w, name);
+	diffWidgetOutput(name);
 }
 
 QTEST_MAIN(Test)
