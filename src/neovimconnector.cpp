71bf7ef4c918f0abf5fbceba8f92f0f7c57f0e99
jdg.gehrig@gmail.com <jdg.gehrig@gmail.com>
2020-09-03 19:51:21 -0400

    Fix Clazy Warning: clazy-qstring-ref
    Prevents a string copy by calling QStringRef where possible.

--- src/neovimconnector.cpp
+++ src/neovimconnector.cpp
@@ -353,7 +353,7 @@ NeovimConnector* NeovimConnector::connectToNeovim(const QString& server)
 	int colon_pos = addr.lastIndexOf(':');
 	if (colon_pos != -1 && colon_pos != 0 && addr[colon_pos-1] != ':') {
 		bool ok;
-		int port = addr.mid(colon_pos+1).toInt(&ok);
+		int port = addr.midRef(colon_pos+1).toInt(&ok);
 		if (ok) {
 			QString host = addr.mid(0, colon_pos);
 			return connectToHost(host, port);
