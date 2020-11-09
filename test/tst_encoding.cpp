0f1a92d51972526ced1af98d4e3a2b9ef84becdb
jdg.gehrig@gmail.com <jdg.gehrig@gmail.com>
2020-11-09 00:33:46 -0500

    Fix Clazy Warning: clazy-lambda-in-connect
    Lambda reference captures within a connect may outlive their scope.

--- test/tst_encoding.cpp
+++ test/tst_encoding.cpp
@@ -4,6 +4,8 @@
 #include <neovimconnector.h>
 #include <util.h>
 
+#include "common.h"
+
 class TestEncoding: public QObject
 {
 	Q_OBJECT
@@ -12,32 +14,22 @@ private slots:
 	void encodeString();
 	void map();
 	void stringsAreBinaryNotUtf8();
+
 protected:
-	NeovimQt::NeovimConnector *m_c;
+	NeovimQt::NeovimConnector* m_c;
 };
 
 void TestEncoding::encodeString()
 {
 	QMetaObject::Connection conn;
-	m_c->neovimObject()->vim_set_var("testing-neovim-qt", QString("value"));
-	m_c->neovimObject()->vim_get_var("testing-neovim-qt");
-	conn = connect(m_c->neovimObject(), &NeovimQt::NeovimApi1::on_vim_get_var,
+	m_c->api1()->vim_set_var("testing-neovim-qt", QString("value"));
+	m_c->api1()->vim_get_var("testing-neovim-qt");
+	QSignalSpy onVimGetVar{ m_c->api1(), &NeovimQt::NeovimApi1::on_vim_get_var };
+	conn = connect(m_c->api1(), &NeovimQt::NeovimApi1::on_vim_get_var,
 			[](const QVariant& v) {
 				QVERIFY(v == QByteArray("value"));
 			});
-	QTest::qWait(500);
-	disconnect(conn);
-
-	bool var_set = false;
-	m_c->neovimObject()->vim_set_var("testing-neovim-qt", QByteArray("value"));
-	m_c->neovimObject()->vim_get_var("testing-neovim-qt");
-	conn = connect(m_c->neovimObject(), &NeovimQt::NeovimApi1::on_vim_get_var,
-			[&var_set](const QVariant& v) {
-				var_set = true;
-				QVERIFY(v == QVariant(QByteArray("value")));
-			});
-	QTest::qWait(500);
-	QVERIFY(var_set);
+	QVERIFY(SPYWAIT(onVimGetVar));
 	disconnect(conn);
 }
 
@@ -46,13 +38,20 @@ void TestEncoding::map()
 	QMetaObject::Connection conn;
 	QVariantMap map;
 	map.insert("answer", 42);
-	m_c->neovimObject()->vim_set_var("test-map", map);
-	m_c->neovimObject()->vim_get_var("test-map");
-	conn = connect(m_c->neovimObject(), &NeovimQt::NeovimApi1::on_vim_get_var,
+
+	m_c->api1()->vim_set_var("test-map", map);
+	QSignalSpy onVimSetVar{ m_c->api1(), &NeovimQt::NeovimApi1::on_vim_set_var };
+	QVERIFY(SPYWAIT(onVimSetVar));
+
+	m_c->api1()->vim_get_var("test-map");
+	QSignalSpy onVimGetVar{ m_c->api1(), &NeovimQt::NeovimApi1::on_vim_get_var };
+	conn = connect(m_c->api1(), &NeovimQt::NeovimApi1::on_vim_get_var,
 			[map](const QVariant& v) {
+				qDebug() << "Expected:" << map;
+				qDebug() << "Observed:" << v;
 				QVERIFY(v == map);
 			});
-	QTest::qWait(500);
+	QVERIFY(SPYWAIT(onVimGetVar));
 	disconnect(conn);
 }
 
@@ -61,28 +60,29 @@ void TestEncoding::map()
 void TestEncoding::stringsAreBinaryNotUtf8()
 {
 	QByteArray data = "\xc3\x28";
-	m_c->neovimObject()->vim_set_current_line(data);
-	m_c->neovimObject()->vim_get_current_line();
+
+	m_c->api1()->vim_set_current_line(data);
+	QSignalSpy onVimSetCurrentLine{ m_c->api1(), &NeovimQt::NeovimApi1::on_vim_set_current_line};
+	QVERIFY(SPYWAIT(onVimSetCurrentLine));
+
+	m_c->api1()->vim_get_current_line();
 	QMetaObject::Connection conn;
-	conn = connect(m_c->neovimObject(), &NeovimQt::NeovimApi1::on_vim_get_current_line,
+	QSignalSpy onVimGetCurrentLine{ m_c->api1(), &NeovimQt::NeovimApi1::on_vim_get_current_line};
+	conn = connect(m_c->api1(), &NeovimQt::NeovimApi1::on_vim_get_current_line,
 			[data](const QVariant& v) {
 				QCOMPARE(v.toByteArray(), data);
 			});
-	QTest::qWait(500);
+	QVERIFY(SPYWAIT(onVimGetCurrentLine));
 	disconnect(conn);
 }
 
 void TestEncoding::initTestCase()
 {
-	bool ready = false;
 	m_c = NeovimQt::NeovimConnector::spawn({"-u", "NONE"});
 	QVERIFY(m_c->errorCause() == NeovimQt::NeovimConnector::NoError);
-	connect(m_c, &NeovimQt::NeovimConnector::ready,
-		[&ready](){
-			ready = true;
-		});
-	QTest::qWait(1500);
-	QVERIFY(ready);
+	QSignalSpy onReady(m_c, SIGNAL(ready()));
+	QVERIFY(onReady.isValid());
+	QVERIFY(SPYWAIT(onReady));
 }
 
 QTEST_MAIN(TestEncoding)
