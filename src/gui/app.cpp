8e167e15d32e9df615bc8c05de74fecb2d1c651b
statiolake <statiolake@gmail.com>
2020-12-26 03:35:58 +0900

    Issue 806: Remove hard coded NVIM_QT_RUNTIME_PATH
    Hard coding the compile-time install path is sub-optimal.
    
    This can cause issues when the intended install location is changed. For
    example the program is compiled on Windows with D:\Program Files. The
    users installs to C:\Program Files.
    
    This can impact Linux too, but is less likely: extract package image to /opt.
    
    This has already been removed for MacOS. Removing for Linux/Windows
    allows us to use the same code for all platforms.

--- src/gui/app.cpp
+++ src/gui/app.cpp
@@ -254,7 +254,24 @@ void App::setupRequestTimeout() noexcept
 	m_connector->setRequestTimeout(m_parser.value("timeout").toInt());
 }
 
-QStringList App::getNeovimArgs() noexcept
+/*static*/ QString App::getRuntimePath() noexcept
+{
+	QString path{ QString::fromLocal8Bit(qgetenv("NVIM_QT_RUNTIME_PATH")) };
+
+	if (QFileInfo(path).isDir()) {
+		return path;
+	}
+
+	// Look for the runtime relative to the nvim-qt binary
+	const QDir d{ QDir{ applicationDirPath() }.filePath(NVIM_QT_RELATIVE_RUNTIME_PATH) };
+	if (d.exists()) {
+		return d.path();
+	}
+
+	return {};
+}
+
+/*static*/ QStringList App::getNeovimArgs() noexcept
 {
 	QStringList neovimArgs{ "--cmd","set termguicolors" };
 
