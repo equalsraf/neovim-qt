12d97427fa6aec8e41d7b6d9b0445d5ab32eac89
Helmut Januschka <helmut@januschka.com>
2021-04-18 22:36:29 +0200

    address feedback

--- test/tst_input_mac.cpp
+++ test/tst_input_mac.cpp
@@ -23,7 +23,6 @@ void TestInputMac::GermanKeyboard() noexcept
 	QLocale* locale_ref = &test_locale;
 
 	/// Check if special characters work
-
 	std::vector<QChar> removeAltCharList  { '[', ']', '|', '{', '}', '~', '@', '\'' };
 	
 	// Verify that we have a DE_de keyboard
@@ -38,23 +37,18 @@ void TestInputMac::GermanKeyboard() noexcept
 	
 	QKeyEvent evOption7{ QEvent::KeyPress, Qt::Key_7, Qt::AltModifier, "|" };
 	QCOMPARE(NeovimQt::Input::convertKey(evOption7, locale_ref), QString{ "|" });
-	
 
 	QKeyEvent evOption8{ QEvent::KeyPress, Qt::Key_8, Qt::AltModifier, "{" };
 	QCOMPARE(NeovimQt::Input::convertKey(evOption8, locale_ref), QString{ "{" });
 	
 	QKeyEvent evOption9{ QEvent::KeyPress, Qt::Key_9, Qt::AltModifier, "}" };
 	QCOMPARE(NeovimQt::Input::convertKey(evOption9, locale_ref), QString{ "}" });
-	
 
 	QKeyEvent evOptionTilde{ QEvent::KeyPress, Qt::Key_N, Qt::AltModifier, "~" };
 	QCOMPARE(NeovimQt::Input::convertKey(evOptionTilde, locale_ref), QString{ "~" });
 
-
 	QKeyEvent evOptionAtSign{ QEvent::KeyPress, Qt::Key_L, Qt::AltModifier, "@" };
 	QCOMPARE(NeovimQt::Input::convertKey(evOptionAtSign, locale_ref), QString{ "@" });
-
-
 }
 
 void TestInputMac::AltSpecialCharacters() noexcept
