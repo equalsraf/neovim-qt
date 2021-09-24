6d5cce86e966e679cc521496d7c06b5ab5e01e4c
John Gehrig <jdg.gehrig@gmail.com>
2021-09-23 23:40:10 -0400

    Remove tst_shell ifdef statements

--- test/tst_shell.h
+++ test/tst_shell.h
@@ -1,8 +1,14 @@
 #pragma once
 
+#include <QClipboard>
+#include <QString>
 #include <QStringList>
 
 namespace NeovimQt {
+
+QClipboard::Mode GetClipboardMode(char reg) noexcept;
+
+QString GetPlatformTestFont() noexcept;
 
 QStringList BinaryAndArgumentsNoForkWithCommand(const QString& command) noexcept;
 
