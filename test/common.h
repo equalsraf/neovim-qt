c67f266273361c1bafcec9125bf35a629fb2a628
John Gehrig <jdg.gehrig@gmail.com>
2021-09-23 23:11:39 -0400

    Cleanup for test/common.h

--- test/common.h
+++ test/common.h
@@ -1,11 +1,10 @@
-#ifndef NEOVIM_QT_TEST_COMMON
-#define NEOVIM_QT_TEST_COMMON
+#pragma once
+
+#include <QSignalSpy>
 
 // This is just a fix for QSignalSpy::wait
 // http://stackoverflow.com/questions/22390208/google-test-mock-with-qt-signals
-bool SPYWAIT(QSignalSpy &spy, int timeout=30000)
+bool SPYWAIT(QSignalSpy& spy, int timeout = 30000)
 {
-	return spy.count()>0||spy.wait(timeout);
+	return spy.count() > 0 || spy.wait(timeout);
 }
-
-#endif
