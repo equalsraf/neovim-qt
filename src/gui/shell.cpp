equalsraf/neovim-qt

commit 2c3c7c292bb6fc7701bda76343458ec0d1a9bbd4

Fix linespace option, shared code with GuiLinespace
Setting linespace via `GuiLinespace` works, but `set linespace=` does not.

The resize event is not called for the later. Update both callers to use the
same code, `Shell::handleLineSpace`.

src/gui/shell.cpp
@@ -768,12 +768,7 @@ void Shell::handleNeovimNotification(const QByteArray &name, const QVariantList&
 				emit neovimFullScreen(variant_not_zero(args.at(1)));
 			}
 		} else if (guiEvName == "Linespace" && args.size() == 2) {
-			// The conversion to string and then to int happens because of http://doc.qt.io/qt-5/qvariant.html#toUInt
-			// toUint() fails to detect an overflow i.e. it converts to ulonglong and then returns a MAX UINT
-			auto val = args.at(1).toString().toInt();
-			setLineSpace(val);
-			m_nvim->api0()->vim_set_var("GuiLinespace", val);
-			resizeNeovim(size());
+			handleLineSpace(args.at(1));
 		} else if (guiEvName == "Mousehide" && args.size() == 2) {
 			m_mouseHide = variant_not_zero(args.at(1));
 			int val = m_mouseHide ? 1 : 0;
@@ -860,9 +855,7 @@ void Shell::handleSetOption(const QString& name, const QVariant& value)
 	} else if (name == "guifontwide") {
 		handleGuiFontWide(value);
 	} else if (name == "linespace") {
-		// The conversion to string and then to int happens because of http://doc.qt.io/qt-5/qvariant.html#toUInt
-		// toUint() fails to detect an overflow i.e. it converts to ulonglong and then returns a MAX UINT
-		setLineSpace(value.toString().toInt());
+		handleLineSpace(value);
 	} else if (name == "showtabline") {
 		emit neovimShowtablineSet(value.toString().toInt());
 	} else if (name == "ext_tabline") {
@@ -918,6 +911,21 @@ void Shell::handleGuiFontWide(const QVariant& value) noexcept
 	const QString fdesc{ m_nvim->decode(value.toByteArray()) };
 
 	setGuiFontWide(fdesc);
+}
+
+void Shell::handleLineSpace(const QVariant& value) noexcept
+{
+	bool isValid{ false };
+	int linespace{ value.toInt(&isValid) };
+
+	if (!isValid || linespace < 0) {
+		qDebug() << "Invalid GuiLinespace value:" << value;
+		return;
+	}
+
+	setLineSpace(linespace);
+	m_nvim->api0()->vim_set_var("GuiLinespace", linespace);
+	resizeNeovim(size());
 }
 
 void Shell::handleGridResize(const QVariantList& opargs)

src/gui/shell.h
@@ -161,6 +161,7 @@ protected:
 	virtual void handleMouse(bool);
 	virtual void handleGuiFontFunction(const QVariantList& args);
 	virtual void handleGuiFontWide(const QVariant& value) noexcept;
+	virtual void handleLineSpace(const QVariant& value) noexcept;
 
 	// Modern 'ext_linegrid' Grid UI Events
 	virtual void handleGridResize(const QVariantList& opargs);

commit e42439906d771c736845ba277a5359ef56213a14

Issue 981: Allow negative linespace
We should allow negative linespace, this was accidentally regressed in
commit 2c3c7c2. Removes the invalid < 0 check.

src/gui/shell.cpp
@@ -998,7 +998,7 @@ void Shell::handleLineSpace(const QVariant& value) noexcept
 	bool isValid{ false };
 	int linespace{ value.toInt(&isValid) };
 
-	if (!isValid || linespace < 0) {
+	if (!isValid) {
 		qDebug() << "Invalid GuiLinespace value:" << value;
 		return;
 	}