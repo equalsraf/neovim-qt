6dcf82fe5a19d1f9af6b71e13e976d13f61f7515
twelveeighty <twelve.eighty@gmail.com>
2026-04-04 09:09:20 -0600

    Satisfy clang 15 formatting

--- src/msgpackiodevice.cpp
+++ src/msgpackiodevice.cpp
@@ -738,73 +738,73 @@ void MsgpackIODevice::send(const QVariant& var)
 	switch (var.metaType().id()) {
 		case QMetaType::Void:
 		case QMetaType::UnknownType:
-			msgpack_pack_nil(&m_pk);
-			break;
+		msgpack_pack_nil(&m_pk);
+		break;
 		case QMetaType::Bool:
-			send(var.toBool());
-			break;
+		send(var.toBool());
+		break;
 		case QMetaType::Int:
-			msgpack_pack_int(&m_pk, var.toInt());
-			break;
+		msgpack_pack_int(&m_pk, var.toInt());
+		break;
 		case QMetaType::UInt:
-			msgpack_pack_unsigned_int(&m_pk, var.toUInt());
-			break;
+		msgpack_pack_unsigned_int(&m_pk, var.toUInt());
+		break;
 		case QMetaType::Long:
-			msgpack_pack_long_long(&m_pk, var.toLongLong());
-			break;
+		msgpack_pack_long_long(&m_pk, var.toLongLong());
+		break;
 		case QMetaType::LongLong:
-			msgpack_pack_long_long(&m_pk, var.toLongLong());
-			break;
+		msgpack_pack_long_long(&m_pk, var.toLongLong());
+		break;
 		case QMetaType::ULong:
-			msgpack_pack_unsigned_long_long(&m_pk, var.toULongLong());
-			break;
+		msgpack_pack_unsigned_long_long(&m_pk, var.toULongLong());
+		break;
 		case QMetaType::ULongLong:
-			msgpack_pack_unsigned_long_long(&m_pk, var.toULongLong());
-			break;
+		msgpack_pack_unsigned_long_long(&m_pk, var.toULongLong());
+		break;
 		case QMetaType::Float:
-			msgpack_pack_float(&m_pk, var.toFloat());
-			break;
+		msgpack_pack_float(&m_pk, var.toFloat());
+		break;
 		case QMetaType::Double:
-			msgpack_pack_double(&m_pk, var.toDouble());
-			break;
+		msgpack_pack_double(&m_pk, var.toDouble());
+		break;
 		case QMetaType::QByteArray:
-			send(var.toByteArray());
-			break;
+		send(var.toByteArray());
+		break;
 		case QMetaType::QStringList:
-			msgpack_pack_array(&m_pk, var.toList().size());
-			foreach (const QVariant& elem, var.toList()) {
-				send(elem);
-			}
-			break;
+		msgpack_pack_array(&m_pk, var.toList().size());
+		foreach (const QVariant& elem, var.toList()) {
+			send(elem);
+		}
+		break;
 		case QMetaType::QVariantList:
-			msgpack_pack_array(&m_pk, var.toList().size());
-			foreach (const QVariant& elem, var.toList()) {
-				send(elem);
-			}
-			break;
+		msgpack_pack_array(&m_pk, var.toList().size());
+		foreach (const QVariant& elem, var.toList()) {
+			send(elem);
+		}
+		break;
 		case QMetaType::QVariantMap:
 		{
-			const QVariantMap& m = var.toMap();
-			msgpack_pack_map(&m_pk, m.size());
-			QMapIterator<QString, QVariant> it(m);
-			while (it.hasNext()) {
-				it.next();
-				send(it.key());
-				send(it.value());
-			}
+		const QVariantMap& m = var.toMap();
+		msgpack_pack_map(&m_pk, m.size());
+		QMapIterator<QString, QVariant> it(m);
+		while (it.hasNext()) {
+			it.next();
+			send(it.key());
+			send(it.value());
+		}
 		} break;
 		case QMetaType::QPoint:
-			// As an array [row, col]
-			msgpack_pack_array(&m_pk, 2);
-			msgpack_pack_int64(&m_pk, var.toPoint().y());
-			msgpack_pack_int64(&m_pk, var.toPoint().x());
-			break;
+		// As an array [row, col]
+		msgpack_pack_array(&m_pk, 2);
+		msgpack_pack_int64(&m_pk, var.toPoint().y());
+		msgpack_pack_int64(&m_pk, var.toPoint().x());
+		break;
 		case QMetaType::QString:
-			send(encode(var.toString()));
-			break;
+		send(encode(var.toString()));
+		break;
 		default:
-			msgpack_pack_nil(&m_pk);
-			qWarning() << "There is a BUG in the QVariant serializer" << var.metaType().id();
+		msgpack_pack_nil(&m_pk);
+		qWarning() << "There is a BUG in the QVariant serializer" << var.metaType().id();
 	}
 }
 
@@ -844,56 +844,56 @@ bool MsgpackIODevice::checkVariant(const QVariant& var)
 {
 	switch (var.metaType().id()) {
 		case QMetaType::UnknownType:
-			break;
+		break;
 		case QMetaType::Bool:
-			break;
+		break;
 		case QMetaType::Int:
-			break;
+		break;
 		case QMetaType::UInt:
-			break;
+		break;
 		case QMetaType::Long:
-			break;
+		break;
 		case QMetaType::LongLong:
-			break;
+		break;
 		case QMetaType::ULong:
-			break;
+		break;
 		case QMetaType::ULongLong:
-			break;
+		break;
 		case QMetaType::Float:
-			break;
+		break;
 		case QMetaType::QString:
-			break;
+		break;
 		case QMetaType::Double:
-			break;
+		break;
 		case QMetaType::QByteArray:
-			break;
+		break;
 		case QMetaType::QStringList:
-			break;
+		break;
 		case QMetaType::QVariantList:
-			foreach (const QVariant& elem, var.toList()) {
-				if (!checkVariant(elem)) {
-					return false;
-				}
+		foreach (const QVariant& elem, var.toList()) {
+			if (!checkVariant(elem)) {
+				return false;
 			}
-			break;
+		}
+		break;
 		case QMetaType::QVariantMap:
 		{
-			const QVariantMap& m = var.toMap();
-			QMapIterator<QString, QVariant> it(m);
-			while (it.hasNext()) {
-				it.next();
-				if (!checkVariant(it.key())) {
-					return false;
-				}
-				if (!checkVariant(it.value())) {
-					return false;
-				}
+		const QVariantMap& m = var.toMap();
+		QMapIterator<QString, QVariant> it(m);
+		while (it.hasNext()) {
+			it.next();
+			if (!checkVariant(it.key())) {
+				return false;
 			}
+			if (!checkVariant(it.value())) {
+				return false;
+			}
+		}
 		} break;
 		case QMetaType::QPoint:
-			break;
+		break;
 		default:
-			return false;
+		return false;
 	}
 	return true;
 }
