88bd19bc70e75c8938989b651fa7f15c85255b62
equalsraf <redacted>
2026-07-26 15:14:09 +0100

    Add cmake targets for git clang-format

---
+++ cmake/Format.cmake
@@ -0,0 +1,37 @@
+
+function(ClangFormat)
+	find_program(GIT_PROGRAM git)
+	find_program(CLANGFORMAT_PROGRAM clang-format)
+
+	if(GIT_PROGRAM AND CLANGFORMAT_PROGRAM)
+		execute_process(
+			COMMAND ${GIT_PROGRAM} clang-format -h
+			WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+			RESULT_VARIABLE GITHELPER_STATUS
+			OUTPUT_QUIET)
+	endif()
+
+	set(COMMON_ARGS --style file:${PROJECT_SOURCE_DIR}/contrib/clang-format.txt origin/master)
+
+	if(GITHELPER_STATUS EQUAL 0)
+		message(STATUS "Enabling format target")
+
+		add_custom_target(format
+			WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+			COMMAND ${GIT_PROGRAM} clang-format ${COMMON_ARGS})
+		add_custom_target(formatdiff
+			WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+			COMMAND ${GIT_PROGRAM} clang-format --diff ${COMMON_ARGS})
+	else()
+		message(STATUS "format: Failed to run git clang-format: ${GITHELPER_STATUS}")
+
+		set(COMMAND_ARGS
+			# show error message & fail
+			COMMAND ${CMAKE_COMMAND} -E echo
+			"format target requires git/clang-format with a working git clang-format helper"
+			COMMAND ${CMAKE_COMMAND} -E false)
+
+		add_custom_target(format ${COMMAND_ARGS})
+		add_custom_target(formatdiff ${COMMAND_ARGS})
+	endif()
+endfunction()
