0afca139bfb16f03c022fa2504439d57348de29f
equalsraf <redacted>
2026-08-15 11:40:36 +0100

    Refactor cmake format to be called without a project

--- cmake/Format.cmake
+++ cmake/Format.cmake
@@ -1,5 +1,16 @@
+function(FormatCommand GITPRG PROJECTDIR BASEREF DIFF OUTVAR)
+	set(COMMON_ARGS --style file:${PROJECTDIR}/contrib/clang-format.txt ${BASEREF})
 
-function(ClangFormat BASEREF)
+	set(FLAGS)
+	if(DIFF)
+		list(APPEND FLAGS --diff)
+	endif()
+
+	set(${OUTVAR} ${GITPRG} clang-format ${FLAGS} ${COMMON_ARGS} PARENT_SCOPE)
+endfunction()
+
+
+function(ClangFormatTargets BASEREF)
 	find_program(GIT_PROGRAM git)
 	find_program(CLANGFORMAT_PROGRAM clang-format)
 
@@ -16,15 +27,20 @@ function(ClangFormat BASEREF)
 	if(GITHELPER_STATUS EQUAL 0)
 		message(STATUS "Enabling format target against ${BASEREF}")
 
+
+		FormatCommand(${GIT_PROGRAM} ${PROJECT_SOURCE_DIR} ${BASEREF} FALSE CMD)
+		message(DEBUG "format cmd: ${CMD}")
+		FormatCommand(${GIT_PROGRAM} ${PROJECT_SOURCE_DIR} ${BASEREF} TRUE DIFFCMD)
+		message(DEBUG "format diff cmd: ${DIFFCMD}")
+
 		add_custom_target(format
 			WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-			COMMAND ${GIT_PROGRAM} clang-format ${COMMON_ARGS})
+			COMMAND ${CMD})
 		add_custom_target(formatdiff
 			WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-			COMMAND ${GIT_PROGRAM} clang-format --diff ${COMMON_ARGS})
+			COMMAND ${DIFFCMD})
 	else()
 		message(STATUS "format: Failed to run git clang-format: ${GITHELPER_STATUS}")
-
 		set(COMMAND_ARGS
 			# show error message & fail
 			COMMAND ${CMAKE_COMMAND} -E echo
