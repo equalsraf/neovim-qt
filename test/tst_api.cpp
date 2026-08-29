3dd481a8650f1c826aed0e120f8cc4674847707f
equalsraf <redacted>
2026-08-29 15:56:49 +0100

    Skip functions due to the LuaRef type
    The LuaRef type is not really useful for an RPC api caller. Previously
    it was encoded as NIL, and in the latest upstream it is encoded as a
    helper string, but not useful as an argument.

---
+++ test/tst_api.cpp
@@ -0,0 +1,62 @@
+#include <QLocalSocket>
+#include <QMetaMethod>
+#include <QtTest/QtTest>
+#include <neovimconnector.h>
+#include <msgpackrequest.h>
+#include "common.h"
+
+class TestApi: public QObject
+{
+	Q_OBJECT
+
+private slots:
+	void initTestCase();
+	void nvim_get_keymap();
+	void execute_lua();
+	void nvim_buf_call();
+
+private:
+	NeovimQt::NeovimConnector *m_c;
+};
+
+void TestApi::initTestCase()
+{
+	m_c = NeovimQt::NeovimConnector::spawn({"-u", "NORC"});
+	QSignalSpy onReady(m_c, SIGNAL(ready()));
+	QVERIFY(onReady.isValid());
+
+	QVERIFY(SPYWAIT(onReady));
+}
+
+
+/**
+ * This only exists to document api behavior - the LuaRef type can be seen in the callback
+ * attribute, but a breaking change means previously this was NIL/Invalid but now it is
+ * a string.
+ *
+ */
+void TestApi::nvim_get_keymap()
+{
+	QVERIFY(m_c->neovimObject());
+	auto *api = m_c->api3();
+
+	auto r = api->nvim_execute_lua("vim.api.nvim_set_keymap('n', '<leader>b', '', {noremap = true, desc = 'Test', callback = function() end})", QVariantList());
+
+	QSignalSpy wait(r, &NeovimQt::MsgpackRequest::finished);
+	QVERIFY(wait.isValid());
+	QVERIFY(SPYWAIT(wait));
+	qDebug() << "nvim_execute_lua" << wait.takeFirst();
+
+
+	QSignalSpy result(api, SIGNAL(on_nvim_get_keymap(QList<QVariantMap>)));
+	QVERIFY(result.isValid());
+
+	api->nvim_get_keymap("n");
+	QVERIFY(SPYWAIT(result));
+
+	auto value = result.takeFirst();
+	qDebug() << "nvim_get_keymap" << value;
+}
+
+QTEST_MAIN(TestApi)
+#include "tst_api.moc"
